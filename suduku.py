87608800e5e8d61f34251988a80d1d00a401289f
Batsal Awaley <batsal@vayu.com.au>
2019-05-24 06:56:32 +0545

    convert suduku to class

--- suduku.py
+++ suduku.py
@@ -1,29 +1,29 @@
-SIZE = 9
-
-board =[[0 for x in range(SIZE)] for y in range(SIZE)]
-
-def initializeBoard(board, inputs):
-	i = 0
-	j = 0
-	for index, data in enumerate(inputs):
-		i = index // 9
-		j = index % 9
-		board[i][j] = data
-
-def printBoard(board):
-	i = 0
-	while i < SIZE:
-		row = ""
+class Suduku:
+	SIZE = 9
+	def __init__(self, inputs):
+		self.board =[[0 for x in range(self.SIZE)] for y in range(self.SIZE)]
+		i = 0
 		j = 0
-		while j < SIZE:
-			row += '|' if (j % 3  == 0) else ' '
-			row += '-'  if (board[i][j] == 0) else str(board[i][j])
-			j += 1
-		row+=  '|'
-		if(i % 3 == 0 ):
-			print('-------------------')
-		print(row)
-		i += 1
+		for index, data in enumerate(inputs):
+			i = index // 9
+			j = index % 9
+			self.board[i][j] = data
+	def printBoard(self):
+		i = 0
+		while i < self.SIZE:
+			row = ""
+			j = 0
+			while j < self.SIZE:
+				row += '|' if (j % 3  == 0) else ' '
+				row += '-'  if (self.board[i][j] == 0) else str(self.board[i][j])
+				j += 1
+			row+=  '|'
+			if(i % 3 == 0 ):
+				print('-------------------')
+			print(row)
+			i += 1
+
+	# def check_number(self, i, j, number):
 
 
 initial_data = \
@@ -38,6 +38,8 @@ initial_data = \
 		0, 0, 0, 1, 0, 0, 0, 0, 4,\
 		0, 9, 6, 0, 0, 2, 5, 1, 0\
 	]
-initializeBoard(board, initial_data)
+s1 = Suduku(initial_data)
+s1.printBoard()
+# initializeBoard(board, initial_data)
 
-printBoard(board)
+# printBoard(board)
