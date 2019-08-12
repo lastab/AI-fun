fe94be15e021f932530aeb5c8dc76ac0d9a7fd78
Batsal <batsal.awaley@gmail.com>
2019-08-13 05:59:54 +1000

    Working solve suduku using back tracking

--- suduku.py
+++ suduku.py
@@ -9,6 +9,7 @@ class Suduku:
 			j = index % 9
 			self.board[i][j] = data
 	def printBoard(self):
+		print()
 		i = 0
 		while i < self.SIZE:
 			row = ""
@@ -22,27 +23,29 @@ class Suduku:
 				print('-------------------')
 			print(row)
 			i += 1
+		print()
 	def check_number_already_present(self, i, j, number):
 		if (not self.board[i][j] == 0):
 			return True
-		return self.__check_row_already_present(i, number) or self.__check_column_already_present(j, number) or self.__check_square(i, j, number)
-	def __check_row_already_present(self, i, number):
+		return self.check_row_already_present(i, number) or self.check_column_already_present(j, number) or self.check_square(i, j, number)
+
+	def check_row_already_present(self, i, number):
 		j = 0
 		while j < self.SIZE:
 			if(self.board[i][j] == number):
 				return True
 			j +=1
 		return False
-	def __check_column_already_present(self, j, number):
+	def check_column_already_present(self, j, number):
 		i = 0
 		while i < self.SIZE:
 			if(self.board[i][j] == number):
 				return True
 			i += 1
 		return False
-	def __check_square(self, i, j, number):
-		temp_i = i//3
-		temp_j = j//3
+	def check_square(self, i, j, number):
+		temp_i = i//3 * 3
+		temp_j = j//3 * 3
 		limit_i = temp_i + 3
 		limit_j = temp_j + 3
 		while temp_i < limit_i:
@@ -54,52 +57,78 @@ class Suduku:
 		return False
 
 	def set_number(self, i, j, number):
-		if(self.board[i][j] == 0):
-			self.board[i][j] = number 
+		self.board[i][j] = number 
 
-	def board_filled(self):
-		count = 0
+	# def board_filled(self):
+	# 	count = 0
+	# 	i = 0
+	# 	while i < self.size:
+	# 		j = 0
+	# 		while j < self.size:
+	# 			if(self.board[i][j] == 0):
+	# 				count +=1
+	# 			j += 1
+
+	# 		i += 1
+	# 	return count == 0
+
+	def find_empty(self):
 		i = 0
-		while i < self.size:
+		while i < self.SIZE:
 			j = 0
-			while j < self.size:
+			while j < self.SIZE:
 				if(self.board[i][j] == 0):
-					count +=1
+					return [i, j]
 				j += 1
-
 			i += 1
-		return count > 0
+		return None
+
+
+
 initial_data = \
 	[\
-		0, 0, 1, 9, 8, 4, 7, 6, 0,\
-		6, 0, 0, 0, 5, 7, 0, 0, 0,\
-		8, 0, 7, 0, 1, 0, 0, 0, 0,\
-		9, 6, 0, 3, 0, 8, 1, 0, 5,\
-		1, 8, 5, 0, 2, 0, 0, 7, 3,\
-		3, 0, 0, 0, 0, 0, 2, 0, 8,\
-		2, 1, 0, 0, 0, 0, 0, 3, 6,\
-		0, 0, 0, 1, 0, 0, 0, 0, 4,\
-		0, 9, 6, 0, 0, 2, 5, 1, 0\
+		0, 0, 0, 2, 6, 0, 7, 0, 1,\
+		6, 8, 0, 0, 7, 0, 0, 9, 0,\
+		1, 9, 0, 0, 0, 4, 5, 0, 0,\
+		8, 2, 0, 1, 0, 0, 0, 4, 0,\
+		0, 0, 4, 6, 0, 2, 9, 0, 0,\
+		0, 5, 0, 0, 0, 3, 0, 2, 8,\
+		0, 0, 9, 3, 0, 0, 0, 7, 4,\
+		0, 4, 0, 0, 5, 0, 0, 3, 6,\
+		7, 0, 3, 0, 1, 8, 0, 0, 0\
 	]
 s1 = Suduku(initial_data)
 s1.printBoard()
 # initializeBoard(board, initial_data)
-while s1.board_filled:
 
-	i = 0
-	while i < 9:
-		j = 0
-		while j < 9:
-			possible_numbers = []
-			number = 1
-			while number < 10:
-				if(not s1.check_number_already_present(i, j, number) == True):
-					possible_numbers.append(number)
-				number  += 1
-			print(i, j, possible_numbers)
-			if(len(possible_numbers) == 1):
-				s1.set_number(i, j, possible_numbers[0])
-			j += 1
-		i += 1
+def solve(s1):
+	find = s1.find_empty()
 
-	s1.printBoard()
+	if not find:
+		return True
+	i, j = find
+
+	ja = j
+	
+	possible_numbers = []
+	number = 1
+
+	
+	while number < 10:
+
+		if(not s1.check_number_already_present(i, j, number) == True):
+			s1.set_number(i, j, number)
+			
+			if solve(s1):
+				return True
+
+		s1.set_number(i, j, 0) # reset value to empty
+		number += 1
+
+	return False
+
+
+solve(s1)
+s1.printBoard()
+
+
